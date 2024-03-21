2348ef1e61e464f922efacafd11c115885b0ba40
TafaniM <marina.tafani@studentfr.ch>
2024-03-21 11:22:41 +0100

    ajout coche "popular categorie" et adaptations dans la boucle

--- front-page.php
+++ front-page.php
@@ -67,22 +67,56 @@ $context['popular'] = Timber::get_posts( $args );
 // get_terms() -> aller chercher les termes d'une taxonomie
 
 
-////
-$args = array(
-    // 'post_type'      => 'categories',
-    // 'posts_per_page' => '-1',
-    'orderby'        => 'date',
-    'order'          => 'DESC',
-    // 'post_status'    => 'publish'
-    'taxonomy'       => 'photo-type', // Spécifie la taxonomie à partir de laquelle récupérer les termes
-    'hide_empty'     => true, // Inclut les termes qui n'ont pas de publications associées
-);
+// ////
+// $args = array(
+//     // 'post_type'      => 'categories',
+//     // 'posts_per_page' => '-1',
+//     'orderby'        => 'date',
+//     'order'          => 'DESC',
+//     // 'post_status'    => 'publish'
+//     'taxonomy'       => 'photo-type', // Spécifie la taxonomie à partir de laquelle récupérer les termes
+//     'hide_empty'     => true, // Inclut les termes qui n'ont pas de publications associées
+// );
 
 
-$context['categories'] = Timber::get_terms( $args );
-////
+// $context['categories'] = Timber::get_terms( $args );
+// ////
 
+$all_photo_types = Timber::get_terms('photo-type');
+$context['photo_types'] = [];
 
+foreach ($all_photo_types as $single_photo_type) {
+    // Vérifie si la catégorie est marquée comme populaire
+    $is_popular = get_field('categories_popular', $single_photo_type);
+    
+    if ($is_popular) { // Continue seulement si la catégorie est populaire
+        $args = [
+            'posts_per_page' => '1',
+            'post_type' => 'attachment',
+            'post_mime_type' => 'image',
+            'post_status' => 'inherit',
+            'tax_query' => [
+                [
+                    'taxonomy' => 'photo-type',
+                    'field' => 'id',
+                    'terms' => $single_photo_type->ID,
+                ],
+            ],
+            'order_by' => 'date',
+            'order' => 'ASC',
+        ];
+
+        $image = Timber::get_posts($args);
+
+        if (!empty($image)) {
+            $image = reset($image); // Prend la première image trouvée
+            $single_photo_type->image_url = $image->link();
+            $context['photo_types'][] = $single_photo_type;
+        }
+    }
+}
+
+Timber::render(['front-page.twig'], $context);
 
 
 
